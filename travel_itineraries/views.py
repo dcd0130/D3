da6e2e9f15a85c57f6118b56cc8e2db4b8628a9e
Denise Dingsleder <dcd0130@westminstercollege.edu>
2023-04-10 20:10:41 -0600

    fitfth commit

--- travel_itineraries/views.py
+++ travel_itineraries/views.py
@@ -6,8 +6,8 @@ def travel_itinerary_list(request):
     return render(request, 'travel_itineraries/travel_itinerary_list.html', {'travel_itineraries': travel_itineraries})
 
 def hawaii(request):
-    return render(request, 'hawaii.html')
+    return render(request, 'travel_itineraries/hawaii.html')
 
 def california(request):
-    return render(request, 'california.html')
+    return render(request, 'travel_itineraries/california.html')
 
